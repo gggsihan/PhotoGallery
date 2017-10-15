04839386dfcd3223bb6d8deadd1e9d8fed3aff74
gongsihan <m13132182039@163.com>
2017-10-15 10:31:35 +0800

    style done

--- js/mainFunc.js
+++ js/mainFunc.js
@@ -1,14 +1,19 @@
 $(function(){
-	var $lis=$(".nav-item");
-	$lis.click(function(){
+	var $navlis=$(".nav-item");
+	$navlis.click(function(){
 		$(this).addClass("selected active")
 		.siblings().removeClass("selected active");
+		$(".cd-hero-slider >li").eq($(this).index()).addClass("active")
+		.siblings().removeClass("active");
 	});
-	$lis.hover(function(){
+	$navlis.hover(function(){
 		$(this).addClass("selected");
 	},function(){
 		if(!($(this).hasClass("active"))){
 			$(this).removeClass("selected");
 		}
 	});
+
+	//设定current year
+	$(".tm-copyright-year").text(new Date().getFullYear());
 })
